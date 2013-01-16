2ccd05d69e10750ead38be9545aaea25522f5a2d
guruapp <netvideo@gmail.com>
2013-01-16 03:26:35 -0800

    Starting repos.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RcellApp::Application.config.secret_token = '3ccd943f601936fa56bee638662b04d2b1d5dd099736ba314e6d4c4d571ede5a6fddfb529840364d1c71b11e45a893db91dfeaca8254c7492fcabba0082a9768'
